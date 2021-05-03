2b48a7d044e5bd44beaeb7db9b9e16609cd85e33
DanyaalA <Danny13653@gmail.com>
2021-05-03 13:53:35 +0100

    Added Extra Log Channel

--- utils/logChannel.js
+++ utils/logChannel.js
@@ -5,10 +5,26 @@ module.exports = {
     const serverID = "826449038727184404";
     const channelID = "835467376467116053";
 
+    const talkHere = ["826449038727184404", "835467376467116053"];
+    const chingJo = ["818838130815795241", "838759492829184011"];
+
     client.guilds
-      .fetch(serverID)
+      .fetch(talkHere[0])
       .then(async (guild) => {
-        const logChannel = guild.channels.cache.get(channelID);
+        const logChannel = guild.channels.cache.get(talkHere[1]);
+
+        if (logChannel) {
+          setTimeout(() => {
+            logChannel.send(msg);
+          }, 1500);
+        }
+      })
+      .catch(console.error);
+
+    client.guilds
+      .fetch(chingJo[0])
+      .then(async (guild) => {
+        const logChannel = guild.channels.cache.get(chingJo[1]);
 
         if (logChannel) {
           setTimeout(() => {
