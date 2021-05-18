0ccd3be373a7cf6dfb9b0109ed308a60cbcec0dc
DanyaalA <Danny13653@gmail.com>
2021-05-18 14:27:44 +0100

    Updated Payments

--- commands/payment.js
+++ commands/payment.js
@@ -7,19 +7,18 @@ module.exports = {
     if (methodType == "cashapp") {
       message.channel.send(`Cashapp is currently unavailable`);
     } else if (methodType == "crypto") {
-      message.channel.send(`ETH: 0xB8118058a4d024086A05C582c479E58E4b8B797a`);
-      message.channel.send(`BTC: Ask <@814192395809456178> as it changes alot`);
+      message.channel.send(`ETH: 0xCb3fA82D02751Db19ca9F891D99225FD70bb9c26`);
+      message.channel.send(`BTC: 3BomLGxJbTKJ648hkWzHA9MX6vgfhPX3A9`);
+      message.channel.send(`LTC: MFn7mwJXkCGUVDxKRsLX1f8XSe2hrt916W`);
       message.channel.send(
         `If You have Another Crypto Coin Ask & we will see if we can do it`
       );
     } else if (methodType == "eth") {
-      message.channel.send(`ETH: 0xB8118058a4d024086A05C582c479E58E4b8B797a`);
+      message.channel.send(`ETH: 0xCb3fA82D02751Db19ca9F891D99225FD70bb9c26`);
     } else if (methodType == "btc") {
-      message.channel.send(
-        `BTC: Please Ask <@814192395809456178> as the BTC wallet changes`
-      );
+      message.channel.send(`BTC: 3BomLGxJbTKJ648hkWzHA9MX6vgfhPX3A9`);
     } else if (methodType == "ltc") {
-      message.channel.send(`LTC: MAJpuKoFNryf6AVxWqwPkBcFNmAoqUnAkR`);
+      message.channel.send(`LTC: MFn7mwJXkCGUVDxKRsLX1f8XSe2hrt916W`);
     } else if (methodType == "paypal") {
       message.channel.send(
         `Paypal is only available to our verified customers as alot of new customers end up chargebacking after receiving the work`
