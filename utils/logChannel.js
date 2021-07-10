f7eaedcd8c42e9ba83d1f502a42a0ddeb187b378
DanyaalA <Danny13653@gmail.com>
2021-07-10 15:42:01 +0100

    ReWrote Log Function

--- utils/logChannel.js
+++ utils/logChannel.js
@@ -2,36 +2,25 @@ require("dotenv").config();
 
 module.exports = {
   sendLog(client, msg) {
-    const serverID = "826449038727184404";
-    const channelID = "835467376467116053";
+    //ReWrite maybe to find all servers with Log Channel inside bot?
+    const servers = [
+      ["863423914230546462", "863424666052198410"], //Big Baller
+      ["818838130815795241", "838759492829184011"], //ChingJo (Backup)
+    ]; // SERVERID | CHANNELID
 
-    const talkHere = ["826449038727184404", "835467376467116053"];
-    const chingJo = ["818838130815795241", "838759492829184011"];
+    servers.forEach((server) => {
+      client.guilds
+        .fetch(server[0])
+        .then(async (guild) => {
+          const logChannel = guild.channels.cache.get(server[1]);
 
-    client.guilds
-      .fetch(talkHere[0])
-      .then(async (guild) => {
-        const logChannel = guild.channels.cache.get(talkHere[1]);
-
-        if (logChannel) {
-          setTimeout(() => {
-            logChannel.send(msg);
-          }, 1500);
-        }
-      })
-      .catch(console.error);
-
-    client.guilds
-      .fetch(chingJo[0])
-      .then(async (guild) => {
-        const logChannel = guild.channels.cache.get(chingJo[1]);
-
-        if (logChannel) {
-          setTimeout(() => {
-            logChannel.send(msg);
-          }, 1500);
-        }
-      })
-      .catch(console.error);
+          if (logChannel) {
+            setTimeout(() => {
+              logChannel.send(msg);
+            }, 1500);
+          }
+        })
+        .catch(console.error);
+    });
   },
 };
