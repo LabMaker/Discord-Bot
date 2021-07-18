788412418844737c9ca60be0be953b22f22bd7b8
DanyaalA <Danny13653@gmail.com>
2021-07-18 20:21:53 +0100

    Updated API Website

--- utils/APIHelper.js
+++ utils/APIHelper.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const axios = require("axios");
 const fetch = require("node-fetch");
-let domain = "https://reddit-api-bot.herokuapp.com/bot/";
+let domain = "https://reddit-api-bot2.herokuapp.com/bot/";
 
 if (process.env.DEV_MODE) {
   domain = "http://localhost:3000/bot/";
