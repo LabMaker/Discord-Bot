6682698108deb81960e55ef83013509cc049318d
DanyaalA <Danny13653@gmail.com>
2021-07-18 20:17:44 +0100

    Moved Developer Variable to Environment File

--- utils/APIHelper.js
+++ utils/APIHelper.js
@@ -1,10 +1,9 @@
+require("dotenv").config();
 const axios = require("axios");
 const fetch = require("node-fetch");
 let domain = "https://reddit-api-bot.herokuapp.com/bot/";
-let devMode = true;
 
-if (devMode) {
-  devMode = false;
+if (process.env.DEV_MODE) {
   domain = "http://localhost:3000/bot/";
 }
 
