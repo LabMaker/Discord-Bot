720609b4d60bdef433a8ea709fc26368adb4c3f8
DanA <Danny13653@gmail.com>
2021-08-09 19:09:04 +0100

    Replaced Mongoose With API calls

--- src/commands/payment.ts
+++ src/commands/payment.ts
@@ -1,10 +1,10 @@
-import { Message } from "discord.js";
-import Command from "../utils/Base/Command";
-import DiscordClient from "../utils/client";
+import { Message } from 'discord.js';
+import Command from '../utils/Base/Command';
+import DiscordClient from '../utils/client';
 
 export default class Payment extends Command {
   constructor() {
-    super("pay", "Admin", ["payment"]);
+    super('pay', 'Admin', ['payment']);
   }
 
   async run(client: DiscordClient, message: Message, args: string[]) {}
