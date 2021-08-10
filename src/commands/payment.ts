48e00171effcc594287ed7323e69aa0e1f17d701
DanA <Danny13653@gmail.com>
2021-08-10 21:38:38 +0100

    Finished Main Ticket Handler

--- src/commands/payment.ts
+++ src/commands/payment.ts
@@ -1,4 +1,5 @@
 import { Message } from 'discord.js';
+import { GuildConfigDto } from '../data/dtos/guildConfig.dto';
 import Command from '../utils/Base/Command';
 import DiscordClient from '../utils/client';
 
@@ -7,5 +8,33 @@ export default class Payment extends Command {
     super('pay', 'Admin', ['payment']);
   }
 
-  async run(client: DiscordClient, message: Message, args: string[]) {}
+  async run(
+    client: DiscordClient,
+    message: Message,
+    args: string[],
+    guildConfig: GuildConfigDto
+  ) {
+    const payments = await client.API.DiscordConfig.getPayments(
+      guildConfig.paymentConfigId
+    );
+
+    // let buttonTypes = [];
+    // let types = [];
+
+    // payments.map((payment) => {
+    //   if (!types.includes(payment.type)) {
+    //     let tempButton = new MessageButton()
+    //       .setStyle('blurple')
+    //       .setLabel(payment.type)
+    //       .setID(payment.type);
+
+    //     buttonTypes.push(tempButton);
+    //     types.push(payment.type);
+    //   }
+    // });
+
+    // message.channel.send('Our Payment Methods', {
+    //   buttons: buttonTypes,
+    // });
+  }
 }
